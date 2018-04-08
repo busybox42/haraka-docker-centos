5577b7d2d0fc3973835120f6277d108cea504fd8
Alan <alan.denniston@gmail.com>
2018-04-08 05:09:23 +0000

    Modifying build

--- opt/start.sh
+++ opt/start.sh
@@ -4,11 +4,9 @@
 /usr/sbin/clamd --foreground=true &
 /usr/bin/spamd -x &
 /usr/sbin/opendmarc -c /etc/opendmarc.conf
-# Add your own conf and ssl certs 
-#/usr/sbin/opendkim -x /etc/opendkim.conf
-# TODO
-#/usr/local/bin/dmarc_httpd &
 /usr/bin/redis-server /etc/redis.conf &
+/node_modules/Haraka/bin/haraka -i /srv/haraka
+cp /opt/start.sh_postinstall /opt/start.sh
 /node_modules/Haraka/bin/haraka -c /srv/haraka
 
 if [[ $1 == "-d" ]]; then
